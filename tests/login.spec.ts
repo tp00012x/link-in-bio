db5fb21d90f808c5b4a3c7bd0e2ccc50d3194d29
Anthony Torres <anthony_torres01@yahoo.com>
2024-09-05 16:40:18 -0400

    update github

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -1,7 +1,14 @@
 import { test, expect } from "@playwright/test";
 
 test("logging in", async ({ page }) => {
-  await page.goto("/");
+  await page.goto("/", { waitUntil: "networkidle", timeout: 60000 });
+
+  page.on("console", (msg) => console.log("PAGE LOG:", msg.text()));
+  page.on("pageerror", (err) => console.log("PAGE ERROR:", err.message));
+
+  // Log the page content to check if the page is rendered
+  const pageContent = await page.content();
+  console.log(pageContent);
 
   await page.getByPlaceholder("name@company.com").fill("anthony@sexycode.io");
   await page.getByPlaceholder("Enter your password").fill("motivation12");
